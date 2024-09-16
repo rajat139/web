a566a10da34c14ee66d198858e060ce7ea42b526
rajat139 <rajatsaxena139@gmail.com>
2024-09-16 10:23:24 +0530

    Added new features and updated project code

--- src/main/java/com/example/web/service/implement/ClubServiceImpl.java
+++ src/main/java/com/example/web/service/implement/ClubServiceImpl.java
@@ -5,10 +5,12 @@ import com.example.web.models.Club;
 import com.example.web.repository.ClubRepository;
 import com.example.web.service.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class ClubServiceImpl implements ClubService {
     private ClubRepository clubRepository;
 

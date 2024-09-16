bb03c14e054a609994b6cfa0e8b8317833eebd45
rajat139 <rajatsaxena139@gmail.com>
2024-09-16 09:26:52 +0530

    Added new features and updated project code

--- src/main/java/com/example/web/service/ClubService.java
+++ src/main/java/com/example/web/service/ClubService.java
@@ -1,4 +1,9 @@
 package com.example.web.service;
 
+import com.example.web.dto.ClubDto;
+
+import java.util.List;
+
 public interface ClubService {
+    List<ClubDto> findAllClubs();
 }
---
+++ src/main/java/com/example/web/service/implement/ClubServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.web.service.implement;
+
+import com.example.web.dto.ClubDto;
+import com.example.web.models.Club;
+import com.example.web.repository.ClubRepository;
+import com.example.web.service.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClubServiceImpl implements ClubService {
+    private ClubRepository clubRepository;
+
+//    @Autowired
+    public ClubServiceImpl(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+
+    @Override
+    public List<ClubDto> findAllClubs() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream().map((club) -> mapToClubDto(club)).collect(Collectors.toList());
+    }
+
+    private ClubDto mapToClubDto(Club club){
+        ClubDto clubDto = ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updateOn(club.getUpdateOn())
+                .build();
+        return clubDto;
+    }
+}

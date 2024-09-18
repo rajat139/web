378d46ef7f68ff1d6242c455db8068f9265ea7a5
rajat139 <rajatsaxena139@gmail.com>
2024-09-18 17:46:50 +0530

    Added new features and updated project code

--- src/main/java/com/example/web/controller/ClubController.java
+++ src/main/java/com/example/web/controller/ClubController.java
@@ -1,11 +1,14 @@
 package com.example.web.controller;
 
 import com.example.web.dto.ClubDto;
+import com.example.web.models.Club;
 import com.example.web.service.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,5 +26,18 @@ public class ClubController {
         List<ClubDto> clubs = clubService.findAllClubs();
         model.addAttribute("clubs", clubs);
         return "clubs-list";
+    }
+
+    @GetMapping("/clubs/new")
+    public String createClubForm(Model model){
+        Club club = new Club();
+        model.addAttribute("club", club);
+        return "clubs-create";
+    }
+
+    @PostMapping("/clubs/new")
+    public String saveClub(@ModelAttribute("club") Club club){
+        clubService.saveClub(club);
+        return "redirect:/clubs";
     }
 }
--- src/main/java/com/example/web/service/ClubService.java
+++ src/main/java/com/example/web/service/ClubService.java
@@ -1,9 +1,11 @@
 package com.example.web.service;
 
 import com.example.web.dto.ClubDto;
+import com.example.web.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+    Club saveClub(Club club);
 }

3e6e69790a9f2ccd3c7e37add7b7eef5e45cfe00
rajat139 <rajatsaxena139@gmail.com>
2024-09-19 22:28:05 +0530

    Added new features and updated project code

--- src/main/java/com/example/web/controller/ClubController.java
+++ src/main/java/com/example/web/controller/ClubController.java
@@ -3,9 +3,11 @@ package com.example.web.controller;
 import com.example.web.dto.ClubDto;
 import com.example.web.models.Club;
 import com.example.web.service.ClubService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,7 +51,12 @@ public class ClubController {
         return "clubs-edit";
     }
     @PostMapping("/clubs/{clubId}/edit")
-    public String updateClub(@PathVariable("clubId") Long clubId, @ModelAttribute("club") ClubDto club){
+    public String updateClub(@PathVariable("clubId") Long clubId,
+                             @Valid @ModelAttribute("club") ClubDto club,
+                             BindingResult result){
+        if(result.hasErrors())
+            return "clubs-edit";
+
         club.setId(clubId);
         clubService.updateClub(club);
         return "redirect:/clubs";

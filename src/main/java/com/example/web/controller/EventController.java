ae8cc1953c7626dbe5b275c778429347d3b09a73
rajat139 <rajatsaxena139@gmail.com>
2024-09-25 17:11:12 +0530

    Added new features and updated project code

--- src/main/java/com/example/web/controller/EventController.java
+++ src/main/java/com/example/web/controller/EventController.java
@@ -1,11 +1,16 @@
 package com.example.web.controller;
 
+import com.example.web.dto.EventDto;
 import com.example.web.models.Event;
 import com.example.web.service.EventService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class EventController {
 
     private EventService eventService;
@@ -20,5 +25,11 @@ public class EventController {
         model.addAttribute("clubId", clubId);
         model.addAttribute("event", event);
         return "events-create";
+    }
+
+    @PostMapping("/events/{clubId}")
+    public String createEvent(@PathVariable("clubId") Long clubId, @ModelAttribute("event") EventDto eventDto, Model model){
+        eventService.createEvent(clubId, eventDto);
+        return "redirect:/clubs/" + clubId;
     }
 }

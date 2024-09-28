4e1baabe4889ec9aec197f1d9484cd1ce47a6663
rajat139 <rajatsaxena139@gmail.com>
2024-09-28 17:45:54 +0530

    Added new features and updated project code

---
+++ src/main/java/com/example/web/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.web.repository;
+
+import com.example.web.models.Role;
+import com.example.web.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
---
+++ src/main/java/com/example/web/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.web.repository;
+
+
+import com.example.web.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
+    UserEntity findByUsername(String userName);
+}
